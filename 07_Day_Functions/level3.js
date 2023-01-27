008e2c9b79666f0732b666ae9c12febc8e6511cb
laiozago <laiozago@gmail.com>
2023-01-26 23:01:13 -0300

    :books: função exercícios

---
+++ 07_Day_Functions/level3.js
@@ -0,0 +1,21 @@
+function userIdGenerator() {
+    const id = Math
+        .random()
+        .toString(36)
+        .slice(-7);
+    // Escolhe uma posição aleatória na string
+    const randomIndex = Math.floor(Math.random() * id.length);
+    // Substitui o caractere na posição escolhida aleatoriamente por uma letra
+    // maiúscula
+    const newId = id.substr(0, randomIndex) + id[randomIndex].toUpperCase() + id.substr(
+        randomIndex + 1
+    );
+    // escolhe outra posição aleatória
+    const randomIndex2 = Math.floor(Math.random() * newId.length);
+    // Substitui o caractere na posição escolhida aleatoriamente por uma letra
+    // minuscula
+    const newId2 = newId.substr(0, randomIndex2) + newId[randomIndex2].toLowerCase() +
+            newId.substr(randomIndex2 + 1);
+    return newId2;
+}
+console.log(userIdGenerator());
