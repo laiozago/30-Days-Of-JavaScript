30dae902e54e8badf921e2f53ec3c636f6cb288b
laiozago <laiozago@gmail.com>
2023-01-27 00:46:07 -0300

    monte carlo function

--- 07_Day_Functions/07_day_starter/scripts/main.js
+++ 07_Day_Functions/07_day_starter/scripts/main.js
@@ -1,2 +1,37 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+//console.log(countries)
+//alert('Open the console and check if the countries has been loaded')
+
+// obtém a referência para o canvas
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+function monteCarloPi(iterations) {
+  let count = 0;
+  // define o tamanho do canvas
+  canvas.width = 500;
+  canvas.height = 500;
+  // desenha um quadrado
+  ctx.strokeRect(0, 0, canvas.width, canvas.height);
+  // desenha um círculo
+  ctx.beginPath();
+  ctx.arc(canvas.width / 2, canvas.height / 2, canvas.width / 2, 0, 2 * Math.PI);
+  ctx.stroke();
+
+  for (let i = 0; i < iterations; i++) {
+    const x = Math.random();
+    const y = Math.random();
+    // verifica se o ponto gerado está dentro do círculo
+    if (x * x + y * y <= 1) {
+      count++;
+      // desenha um ponto verde dentro do círculo
+      ctx.fillStyle = "green";
+      ctx.fillRect(x * canvas.width, y * canvas.height, 5, 5);
+    } else {
+      // desenha um ponto vermelho fora do círculo
+      ctx.fillStyle = "red";
+      ctx.fillRect(x * canvas.width, y * canvas.height, 5, 5);
+    }
+  }
+  return 4 * count / iterations;
+}
+console.log(monteCarloPi(10000));
--- 07_Day_Functions/level3.js
+++ 07_Day_Functions/level3.js
@@ -1,4 +1,4 @@
-function userIdGenerator() {
+/*function userIdGenerator() {
     const id = Math
         .random()
         .toString(36)
@@ -19,3 +19,24 @@ function userIdGenerator() {
     return newId2;
 }
 console.log(userIdGenerator());
+*/
+
+function monteCarloPi(iterations) {
+    // contador para armazenar a quantidade de pontos gerados dentro do círculo
+    let count = 0;
+    // loop para gerar pontos aleatórios
+    for (let i = 0; i < iterations; i++) {
+      // gera valores aleatórios x e y entre 0 e 1
+      const x = Math.random();
+      const y = Math.random();
+      // verifica se o ponto gerado está dentro do círculo
+      if (x * x + y * y <= 1) {
+        count++;
+      }
+    }
+    // calcula a estimativa de pi com base no número de pontos dentro do círculo
+    return 4 * count / iterations;
+  }
+  // chama a função com o número de iterações desejado
+  console.log(monteCarloPi(10000));
+  
